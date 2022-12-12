c3e4039417e0a9ab0eed19758f09823323b908ec
queens kisivuli <queenskisivuli@gmail.com>
2022-12-12 16:51:12 +0300

    fixed results

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -17,13 +17,12 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
+        if result[i] >= i_arg
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
+        i += 1
     end
     result << i_arg if !is_inserted
 end
